4f466a67f700b02fb839ffa7d395801373b16906
André <andre.romoaldo@gmail.com>
2021-02-04 15:00:54 -0300

    Text-Add
    -Pequenas correções.

--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@ namespace Atividade1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("-||| Bem vindo ao registro de pedidos! |||-");
 
             pedido lista = new pedido();
 
@@ -15,23 +16,24 @@ namespace Atividade1
 
                ItemPedido Pedido = new ItemPedido();
 
-               Console.WriteLine("Informe a descrição do pedido:");
+               Console.WriteLine("-Informe a descrição do pedido:");
                Pedido.descricao = Console.ReadLine();
 
-               Console.WriteLine("Informe o valor da unidade:");
+               Console.WriteLine("-Informe o valor da unidade:");
                Pedido.valor_unitario = int.Parse(Console.ReadLine());
+               Console.WriteLine("O valor da unidade de "+Pedido.descricao+" é: R$"+Pedido.valor_unitario+",00");
 
-               Console.WriteLine("Informe a quantidade de itens:");
+               Console.WriteLine("-Informe a quantidade de itens:");
                Pedido.quantidade = int.Parse(Console.ReadLine());
 
                lista.adicionar(Pedido);
 
-               Console.WriteLine("Deseja inserir um novo pedido? (s/n)");
+               Console.WriteLine("-Deseja inserir um novo pedido? (s/n)");
                resposta = Console.ReadLine();
 
                if (resposta == "n") {
 
-                   Console.WriteLine(lista.totalizar());
+                   Console.WriteLine("|| O seu pedido totalizou o valor de: R$"+lista.totalizar()+",00 ||");
                }
 
            } while (resposta == "s");
--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # Registro de Pedidos
  Prog Console Simples - Add Pedido
+
+-- Atividade 1 SENAC --
+
+// Como um primeiro protótipo do projeto, faça um sistema de pedidos de restaurante em um projeto Console:
+
+-Criar uma classe “ItemPedido” com as propriedades “descricao”, “valor_unitario” e “quantidade”.
+-Criar uma classe “Pedido” com uma propriedade privada do tipo lista (List<>) de “ItemPedido” e métodos para incluir item no pedido e para totalizar o pedido – este, retorna um valor Double com o total de todos os itens da lista.
+-Inclua também um construtor em “Pedido” para instanciar uma nova lista na propriedade criada.
+-Crie um programa em que seja possível informar vários itens de um pedido.
+-O usuário informa descrição, valor e quantidade.
+-O programa pergunta se deseja inserir novo pedido. Se sim, usuário pode incluir novo pedido. Se não, o programa exibe o total do pedido. //
+ 
+
